86563f892192af5817d597f37de6d772ecb55acd
Diptanshu Sinha <sinha.diptanshu10@gmail.com>
2021-09-27 12:02:24 +0530

    Week 1 solution uploaded

---
+++ Week1_Solved/computeCost.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def computeCost(X,y,theta):
+    """
+    Take in a numpy array X,y, theta and generate the cost function     of using theta as parameter in a linear regression model
+    """
+    # np.dot(A,B) => gives dot product of A and B
+    # np.power(A,n) => returns array with each element raised to the power n
+    # np.sum(A) => Returns scalar with every element in A summed up
+    m=len(y)
+    predictions=X.dot(theta)
+    square_err=(predictions - y)**2
+    
+    return 1/(2*m) * np.sum(square_err)
---
+++ Week1_Solved/gradientDescent.py
@@ -0,0 +1,22 @@
+import numpy as np
+from computeCost import computeCost
+#returns updated value of theta
+
+def gradientDescent(X,y,theta,alpha,num_iters):
+    """
+    Take in numpy array X, y and theta and update theta by taking num_iters gradient steps
+    with learning rate of alpha
+    
+    return theta updated
+    """
+    
+    m=len(y)
+    for i in range(num_iters):
+        predictions = np.dot(X,theta)
+        error = np.dot(X.T,(predictions -y))
+        descent=alpha * 1/m * error
+        theta= theta-descent
+        if(i%100==0):
+            print('Cost function after ',i,' iterations is ', computeCost(X,y,theta))
+        
+    return theta
